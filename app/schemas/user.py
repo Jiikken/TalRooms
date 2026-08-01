8b6e6561e306f697744ed0f99929b15b6032d93c
Jiikken <Jiken.rf@yandex.ru>
2026-08-01 17:27:50 +0700

    Добавил pydantic схему для возврата информации о пользователе

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,7 +1,20 @@
+from datetime import datetime
+
 from pydantic import BaseModel, EmailStr, Field
+
 
 class UserCreate(BaseModel):
     firstname: str
     lastname: str
     email: EmailStr
     password: str = Field(..., min_length=8)
+
+class UserResponse(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: EmailStr
+    created: datetime
+
+    class Config:
+        from_attributes = True
