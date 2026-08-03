f33b0047d6d8d9bcabb59cef3bd3c079aa8d3ac5
Jiikken <Jiken.rf@yandex.ru>
2026-08-03 12:46:54 +0700

    Разделил ответственность одной функции

--- app/api/security/cookies.py
+++ app/api/security/cookies.py
@@ -4,17 +4,22 @@ from app.core.security.JWT import create_access_token
 from app.schemas.user import UserResponse
 
 
-def update_tokens_login(request: Request, response: Response, user: UserResponse):
-    """Обновление JWT токенов при авторизации"""
-    token = create_access_token({
+def _create_token(user: UserResponse) -> str:
+    """Создание JWT токена"""
+    return create_access_token({
         "email": user.email,
         "user_id": user.id,
         "role": user.access_lvl
     })
 
+def _delete_cookies(request: Request, response: Response):
+    """Удаление ненужных cookies"""
     if request.cookies.get("access_token"):
         response.delete_cookie("access_token")
+    response.delete_cookie("access_token_reg")
 
+def _set_cookies(response: Response, token: str):
+    """Установка cookies"""
     response.set_cookie(
         key="access_token",
         value=token,
@@ -24,4 +29,9 @@ def update_tokens_login(request: Request, response: Response, user: UserResponse
         max_age=1800,
         path="/"
     )
-    response.delete_cookie("access_token_reg")
+
+def update_tokens_login(request: Request, response: Response, user: UserResponse) -> bool:
+    """Обновление JWT токенов при авторизации"""
+    token = _create_token(user)
+    _delete_cookies(request, response)
+    _set_cookies(response, token)
